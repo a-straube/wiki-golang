3e363dde7129be2b8df94d3e4a62100df5f24278
Axel <kurtastraube@gmail.com>
2015-07-30 10:21:06 -0700

    Add save functionality.

--- wiki.go
+++ wiki.go
@@ -30,6 +30,8 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 		t.Execute(w, p)
 }
 
+// Page Handlers:
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
@@ -49,9 +51,18 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	// Body must be converted to byte
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	// http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
